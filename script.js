ab8c3701ae237fc2edbefe4e2719f3247f68eac8
clementine <cleriv.72@gmail.com>
2023-09-11 10:42:17 +0200

    tests clém pour faire fonctionner la to-do list. KO

--- script.js
+++ script.js
@@ -110,23 +110,23 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value="";
-    saveData();
+    // saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        saveData();
+        // saveData();
     } else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        saveData();
+        // saveData();
     }
 }, false);
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML)
-}
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
+// function saveData(){
+//     localStorage.setItem("data", listContainer.innerHTML)
+// }
+// function showTask(){
+//     listContainer.innerHTML = localStorage.getItem("data");
+// }
+// showTask();
